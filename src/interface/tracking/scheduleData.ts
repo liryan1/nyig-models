efb29b51d15da99c430a539513323c8ad2dc05d1
Ryan Li <ryan.li@ny-go.org>
2024-05-27 21:48:24 -0400

    Add start date and number of classes to group tracker schedule data

--- src/interface/tracking/scheduleData.ts
+++ src/interface/tracking/scheduleData.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const zScheduleData = z.object({
   startTime: z.string(), // String in 24 hour "HH:mm" format
   dayOfWeek: z.number().int().min(0).max(6), // integeters in 0 - 6
+  startDate: z.string().optional(), // Date in YYYYMMdd format, optional for backwards compatibility
+  numberOfclasses: z.number().int().min(0).optional(), // optional for backwards compatibility
 });
 
 export type ScheduleData = z.infer<typeof zScheduleData>;
